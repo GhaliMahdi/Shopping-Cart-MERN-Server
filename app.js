a4a1c6844e047b8e3d06fce0eefe791d389d477c
GhaliMahdi <ghali.mahdi99@gmail.com>
2023-01-03 19:13:05 +0100

    Base of server side, using expressjs, Mongoose, CORS and nodemon

---
+++ app.js
@@ -0,0 +1,41 @@
+//Express App
+const express = require('express')
+const app = express()
+const port = 3000
+//Mongoose
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb+srv://mongoose:test@cluster0.ir5gjii.mongodb.net/shopping_cart?retryWrites=true&w=majority')
+.then(() => app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`); 
+})).catch(() => console.log("database connection error"))
+
+// Cors
+const cors = require('cors');
+app.use(cors());
+
+//Front To Back
+app.use(express.json());
+
+//Schemas
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+//Models
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+
+
+app.get('/xxx', async (req, res) => {
+  const instance = new Kitten({
+    name: 'aaa'
+  });
+  await instance.save();
+
+  instance.save(function (err) {
+    // Nothing
+  });
+  res.send("Hello Pagpougga!")
+});
+
